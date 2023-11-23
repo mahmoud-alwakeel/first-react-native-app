4bfc82079969c27fa0c4076d8b766c7ccc5e5d1d
Mahmoud Alwakeel <alwakeel@Mahmouds-MacBook-Pro.local>
2023-11-23 19:39:32 +0200

    pressable, button & scrollVIew

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
 import { setStatusBarBackgroundColor } from "expo-status-bar";
-import { View, Text, Image, ImageBackground } from "react-native";
+import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable } from "react-native";
 const logoImg = require('./assets/adaptive-icon.png')
 
 export default function App() {
   return (
-    <View style={{flex: 1, backgroundColor: "plum", padding: 40}}>
-      <Text>  
-        <Text style={{color: "white"}}> Hello
-        </Text> weka
-      </Text>
-      {/* <Image source={logoImg} style ={{width: 300, height: 300}}/>
-      <Image source={{uri: "https://picsum.photos/300"}} style ={{width: 300, height: 300}}/> */}
-      <ImageBackground source={logoImg} style={{flex: 1}}>
-        <Text>Image background</Text>
-      </ImageBackground>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 40 }}>
+      <ScrollView>
+        <Text>
+          <Text style={{ color: "white" }}> Hello
+          </Text> weka
+        </Text>
+        <Button
+          title="press"
+          onPress={() => console.log("button pressed")}
+          color={"midnightblue"}
+          disabled={false}
+        />
+        <Pressable onPress={() => console.log("image 1 pressed")}>
+          <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        </Pressable>
+        {/* <Image source={{uri: "https://picsum.photos/300"}} style ={{width: 300, height: 300}}/> */}
+        {/* <ImageBackground source={logoImg} style={{flex: 1}}> </ImageBackground> */}
+        <Pressable onPressIn={() => console.log("on press in triggered")} onLongPress={() => console.log("on long press detected")} onPressOut={() => console.log("om press out")}>
+          <Text>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            condimentum, nisl eu ultricies ultricies, nunc nisl aliquam nunc, eget
+            aliquam nisl nunc sit amet nisl. Donec condimentum, nisl eu ultricies
+            ultricies, nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet
+            nisl. Donec condimentum, nisl eu ultricies ultricies, nunc nisl
+            aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec condimentum,
+            nisl eu ultricies ultricies, nunc nisl aliquam nunc, eget aliquam nist
+            nunc sit amet nisl. Donec condimentum, nisl eu ultricies ultricies,
+            nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec
+            condimentum, nisl eu ultricies ultricies, nunc nisl aliquam nunc, eget
+            aliquam nisl nunc sit amet nisl. Donec condimentum, nisl eu ultricies
+            ultricies, nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet
+            nisl. Donec condimentum, nisl eu ultricies ultricies, nunc nisl
+            aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec condimentum,
+            nisl eu ultricies</Text>
+        </Pressable>
+        <Pressable onPress={() => console.log("image 2 pressed")}>
+          <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        </Pressable>
+      </ScrollView>
     </View>
   )
 }
